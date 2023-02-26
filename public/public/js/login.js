ef842aafc3e064db7b41d7fc99211d4240cce617
Seiya Miyazaki <smsmiyazaki@gmail.com>
2023-02-26 18:20:02 -0500

    trying to fix login form js

--- public/public/js/login.js
+++ public/public/js/login.js
@@ -1,25 +1,28 @@
-const loginFormHandler = async (event) => {
+async function loginFormHandler (event){
   event.preventDefault();
 
-  const usernameEl = document.querySelector('#username-input-login').value.trim();
-  const passwordEl = document.querySelector('#password-input-login').value.trim();
+  const usernameEl = document.querySelector('#username-input-login').value();
+  const passwordEl = document.querySelector('#password-input-login').value();
 
  
+  if (usernameEl && passwordEl) {
     const response = await fetch('/api/user/login', {
       method: 'POST',
-      body: JSON.stringify({ 
-      username: usernameEl.value, 
-      password: passwordEl.value, 
-    }),
-      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        username: usernameEl,
+        password: passwordEl
+      }),
+      headers: { 'Content-Type': 'application/json' }
     });
 
     if (response.ok) {
-      document.location.replace('/');
+      document.location.replace('/dashboard');
     } else {
-      alert('Failed to log in.');
+      alert(response.statusText);
     }
-  };
+  }
+}
+
 
 
 document
