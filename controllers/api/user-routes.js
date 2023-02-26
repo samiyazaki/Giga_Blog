598e1071b1f292eb8bd0dd6ecedb502e60fae7d5
Seiya Miyazaki <smsmiyazaki@gmail.com>
2023-02-26 17:25:01 -0500

    Fixing some more dashboard and user-routes in controllers folder

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,59 +1,108 @@
 const router = require("express").Router();
-const {User} = require("../../models");
+const {User, Post, Comment} = require("../../models");
 
-router.post('/', async (req, res) => {
+router.get('/', async (req, res) => {
     try {
-        const newUser = await User.create({
-            username: req.body.username,
-            email: req.body.email,
-            password: req.body.password,
-        });
-        req.session.save(() => {
-            req.session.user_id = newUser.id;
-            req.session.username = newUser.username;
-            req.session.loggedIn = true;
-            res.json(newUser);
-        });
+      const userData = await User.findAll({
+        attributes: { exclude: ['password'] },
+      });
+      res.status(200).json(userData);
     } catch (err) {
-        res.status(500).json(err);
+      res.status(400).json(err);
     }
-});
-
-router.post('/login', async (req, res) => {
+  });
+  
+  router.get('/:id', async (req, res) => {
     try {
-        const user = await User.findOne({
-            where: {
-                username: req.body.username,
+      const userData = await User.findOne({
+        attributes: { exclude: ['password'] },
+        where: { id: req.params.id },
+        include: [
+          {
+            model: Post, attributes: ['id', 'title', 'content', 'created_at'],
+          },
+          {
+            model: Comment,
+            attributes: ['id', 'comment_text', 'created_at'],
+            include: { model: Post, attributes: ['title'],
             },
-    });
-    if (!user) {
-        res.status(400).json({message: "Incorrect username or password, please try again"});
+          },
+          {
+            model: Post,
+            attributes: ['title'],
+          },
+        ],
+      });
+      console.log(userData);
+      if (!userData) {
+        res.status(404).json({ message: `No such user id ${req.params.id}` });
         return;
-    }
-    const validPassword = await user.checkPassword(req.body.password);
-    if (!validPassword) {
-        res.status(400).json({message: "Incorrect username or password, please try again"});
-        return;
-    }
-    req.session.save(() => {
-        req.session.user_id = user.id;
-        req.session.username = user.username;
-        req.session.loggedIn = true;
-        res.json({user, message: "You are now logged in!"});
-    });
+      }
+      res.status(200).json(userData);
     } catch (err) {
-        res.status(400).json(err);
+      res.status(400).json(err);
     }
-});
-
-router.post('/logout', (req, res) => {
-    if (req.session.loggedIn) {
-        req.session.destroy(() => {
-            res.status(204).end();
+  });
+  
+  router.post('/', async (req, res) => {
+    try {
+      const userData = await User.create(req.body);
+      console.table(req.body);
+      req.session.save(() => {
+        req.session.user_id = userData.id;
+        req.session.username = userData.username;
+        req.session.logged_in = true;
+        res
+          .status(201).json({ message: `Successfully created ${userData.username}` });
+      });
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
+  
+  router.post('/login', async (req, res) => {
+    try {
+      const userData = await User.findOne({
+        where: { username: req.body.username },
+      });
+      if (!userData) {
+        res
+          .status(400).json({ message: `${req.body.username} is not a valid username` });
+        return;
+      }
+  
+      const validPassword = await userData.checkPassword(req.body.password);
+  
+      if (!validPassword) {
+        res.status(400).json({ message: 'Incorrect password, please try again' });
+        return;
+      }
+  
+      req.session.save(() => {
+        req.session.user_id = userData.id;
+        req.session.username = userData.username;
+        req.session.logged_in = true;
+  
+        res.json({ user: userData, message: 'You are now logged in!' });
+      });
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
+  
+  router.post('/logout', async (req, res) => {
+    try {
+      if (req.session.logged_in) {
+        const userData = await req.session.destroy(() => {
+          res.status(204).end();
         });
-    } else {
+      } else {
         res.status(404).end();
+  
+      }
+    } catch {
+      res.status(400).end();
     }
-});
+  });
 
 module.exports = router;
--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,46 +1,76 @@
 const router = require("express").Router();
-const { Post, User } = require("../models");
+const { Post, User, Comment } = require("../models");
 const withAuth = require("../utils/auth");
+const sequelize = require("../config/connection");
 
 router.get("/", withAuth, async (req, res) => {
-  try {
-    const postData = await Post.findAll({
-      where: { user_id: req.session.user_id },
-      include: [User],
+  Post.findAll({
+    where: {
+      user_id: req.session.user_id,
+      },
+      attributes: ['id', 'title', 'content', 'created_at'],
+      include: [
+        {
+          model: Comment,
+          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+          include: { model: User, attributes: ['username'] },
+        },
+        {
+          model: User,
+          attributes: ['username'],
+        },
+      ],
+    })
+    .then((dbPostData) => {
+      const posts = dbPostData.map((post) => post.get({ plain: true }));
+      res.render("dashboard", { posts, loggedIn: true, username: req.session.username });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
     });
-
-    const posts = postData.map((post) => post.get({ plain: true }));
-    console.log(posts);
-    res.render("all-posts", {
-      layout: "dashboard",
-      posts,
-    });
-  } catch (err) {
-    res.redirect("login");
-  }
 });
 
-router.get("/new", withAuth, (req, res) => {
-  res.render("new-post", {
-    layout: "dashboard",
-  });
+router.get("/edit/:id", withAuth, (req, res) => {
+  Post.findOne({
+    where: {
+      id: req.params.id,
+    },
+    attributes: ["id", "title", "content", "created_at"],
+    include: [
+      {
+        model: User,
+        attributes: ["username"],
+      },
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+
+
+      },
+    ],
+  })
+    .then((dbPostData) => {
+      if (!dbPostData) {
+        res.status(404).json({ message: "No post found with this id" });
+        return;
+      }
+      const post = dbPostData.get({ plain: true });
+      console.log('sending ' + req.session.username);
+      res.render('edit-post', { post, loggedIn: true, username: req.session.username });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+router.get("/new", (req, res) => {
+  res.render("new-post", {username: req.session.username });
 });
 
-router.get("/edit/:id", withAuth, async (req, res) => {
-  try {
-    const postData = await Post.findByPk(req.params.id, {
-      include: [User],
-    });
-
-    const post = postData.get({ plain: true });
-
-    res.render("edit-post", {
-      layout: "dashboard",
-      post,
-    });
-  } catch (err) {
-    res.redirect("login");
-  }
-});
 
 module.exports = router;
