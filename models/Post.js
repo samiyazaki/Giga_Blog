7d1c62c67642ff57b9ed0067bdb4df4d66afef17
Seiya Miyazaki <smsmiyazaki@gmail.com>
2023-02-26 18:43:47 -0500

    Error handling, double declared Model

--- models/Post.js
+++ models/Post.js
@@ -1,6 +1,6 @@
 const { Model, DataTypes } = require("sequelize");
 const sequelize = require("../config/connection");
-const {Model, DataTypes} = require('sequelize');
+
 
 class Post extends Model {}
 
