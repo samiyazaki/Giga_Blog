3766544c0d824f785bf8282ea8d27ef447cf0665
Seiya Miyazaki <smsmiyazaki@gmail.com>
2023-02-26 18:24:51 -0500

    signup form handler

--- public/public/js/signup.js
+++ public/public/js/signup.js
@@ -1,24 +1,24 @@
-const signupFormHandler = async (event) => {
-    event.preventDefault();
-  
-    const username = document.querySelector('#username-signup').value.trim();
-    const email = document.querySelector('#email-signup').value.trim();
-    const password = document.querySelector('#password-signup').value.trim();
-  
+async function signupFormHandler(event) {
+  event.preventDefault();
+  const username = document.querySelector('#username-signup').value();
+  const email = document.querySelector('#email-signup').value();
 
-      const response = await fetch('/api/users', {
-        method: 'POST',
-        body: JSON.stringify({ username, email, password }),
-        headers: { 'Content-Type': 'application/json' },
-      });
-  
-      if (response.ok) {
-        document.location.replace('/');
-      } else {
-        alert('Failed to sign up.');
-      }
-    };
-
+  if(username && password) {
+    const response = await fetch('/api/user', {
+      method: 'POST',
+      body: JSON.stringify({
+        username,
+        email,
+        }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert(response.statusText);
+    }
+  }
+  }
   document
-  .querySelector('.signup-form')
+  .querySelector('.#signup-form')
   .addEventListener('submit', signupFormHandler);
